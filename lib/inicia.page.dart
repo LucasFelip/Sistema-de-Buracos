5dc3de0c8536b82684ef93f82ac2446ab886e9a8
LucasFelipe <75092912+LucasFelip@users.noreply.github.com>
2021-03-19 21:25:24 -0300

    Alteração da page iniciat
    Eu mudei todos os parametros dentro para iniciar com um paga, esse mapa puxa como centro a localização do Shopping da ilha, logo inicializando com a tela do mapa

--- lib/inicia.page.dart
+++ lib/inicia.page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/controller.page.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 class IniciatPage extends StatefulWidget {
   @override
@@ -10,28 +12,20 @@ class _IniciatPageState extends State<IniciatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Sistemas De Buracos'),
-          actions: [
-            CustomSwitch(),
-          ],
+      appBar: AppBar(
+        backgroundColor: Colors.green[300],
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(-2.5276041008909473, -44.25520529575539),
+          zoom: 17,
         ),
-        body: Padding(
-          padding: EdgeInsets.all(25),
-          child: Column(
-            children: [],
+        layers: [
+          TileLayerOptions(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           ),
-        ));
-  }
-}
-
-class CustomSwitch extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Switch(
-        value: ControllerPage.instace.dartTheme,
-        onChanged: (value) {
-          ControllerPage.instace.changeTheme();
-        });
-  }
+        ],
+      ),
+    );
+  } 
 }
