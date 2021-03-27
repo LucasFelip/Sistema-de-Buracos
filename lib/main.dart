cd6631e009fda8bf7edcdfa7eb4c717dccf66408
LucasFelipe <75092912+LucasFelip@users.noreply.github.com>
2021-03-27 20:49:37 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/controller.page.dart';
-import 'package:flutter_application_1/login.page.dart';
+import 'package:mobile_sistem/models/controller.page.dart';
+import 'package:mobile_sistem/views/cadastro.page.dart';
+import 'package:mobile_sistem/views/inicia.page.dart';
+import 'package:mobile_sistem/views/login.page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  //sistema de app
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
       animation: ControllerPage.instace,
       builder: (context, child) {
         return MaterialApp(
-          theme: ThemeData(
-            primarySwatch: Colors.blueGrey,
-            brightness: ControllerPage.instace.dartTheme
-                ? Brightness.dark
-                : Brightness.light,
-          ),
-          home: LoginPage(),
-        );
+            theme: ThemeData(
+              primarySwatch: Colors.blueGrey,
+              brightness: ControllerPage.instace.dartTheme
+                  ? Brightness.dark
+                  : Brightness.light,
+            ),
+            initialRoute: '/',
+            routes: {
+              '/': (context) => LoginPage(),
+              '/home': (context) => IniciaPage(),
+              '/cadastro': (context) => CadastroPage(),
+            });
       },
     );
   }
